c82c10748d2e6b7b85c86e999edb333828345e47
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2026-04-01 16:16:18 +0000

    update

---
+++ server/src/database/database.providers.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { STUDENT } from 'src/student.entity';
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mysql',
+        host: 'mysql-14737a33-nglthu-4e05.k.aivencloud.com',
+        port: 17237,
+        username: 'avnadmin',
+        password: 'AVNS_mftPAP8cG5l0Ih_cfL2',
+        database: 'STUDENTSREG',
+        entities: [STUDENT],
+        synchronize: true,
+        ssl: {
+          ca: fs
+            .readFileSync(path.join(__dirname, '..', 'assets', 'ca.pem'))
+            .toString(),
+        },
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
